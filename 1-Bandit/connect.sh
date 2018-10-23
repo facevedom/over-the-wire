d7ea9c8745af00a0a4aa9d3e0c345745c0295183
Felipe Acevedo <felipe.a.montoya@gmail.com>
2018-10-22 23:11:02 -0500

    uses sshpass to inject passwords

--- 1-Bandit/connect.sh
+++ 1-Bandit/connect.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-ssh -p $REMOTE_PORT $REMOTE_USER@$REMOTE_HOST 'bash -s' < $SCRIPT
+sshpass -p $REMOTE_PASSWORD ssh -p $REMOTE_PORT $REMOTE_USER@$REMOTE_HOST 'bash -s' < $SCRIPT
--- 1-Bandit/hack_away.sh
+++ 1-Bandit/hack_away.sh
@@ -4,6 +4,7 @@ export REMOTE_HOST=bandit.labs.overthewire.org
 # level 0
 echo "Hacking level 0"
 export REMOTE_USER=bandit0
+export REMOTE_PASSWORD=bandit0
 export SCRIPT="level0/hack.sh"
 export LEVEL_1_PASS=$(./connect.sh)
 echo "Password obtained for level 1: $LEVEL_1_PASS"
