457d89452b9f54db7f2f7d7573e436a40db3c94c
Felipe Acevedo Montoya <felipe.a.montoya@gmail.com>
2018-10-23 11:57:15 -0500

    refactors entrypoint

--- 1-Bandit/hack_away.sh
+++ 1-Bandit/hack_away.sh
@@ -1,8 +1,17 @@
+#! /bin/bash
+
+export WARGAME="bandit"
+
+give_level_info () {
+    echo "Hacking $WARGAME level $1"
+    echo "Website: http://overthewire.org/wargames/$WARGAME/$WARGAME$1.html"
+}
+
 export REMOTE_PORT=2220
 export REMOTE_HOST=bandit.labs.overthewire.org
 
 # level 0
-echo "Hacking level 0"
+give_level_info 0
 export REMOTE_USER=bandit0
 export REMOTE_PASSWORD=bandit0
 export SCRIPT="level0/steps.sh"
---
+++ README.md
@@ -0,0 +1,13 @@
+# Over the Wire
+## Wargames
+My scripts to solve [Wargames from Over the Wire](http://overthewire.org/wargames/)
+
+## Requirements
+- Bash
+- sshpass
+
+## Usage
+```bash
+cd 1-Bandit
+bash hack_away.sh
+```
