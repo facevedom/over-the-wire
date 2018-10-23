91a8032da91152c693a64121546eda52999284e4
Felipe Acevedo <felipe.a.montoya@gmail.com>
2018-10-22 23:12:58 -0500

    renames level0 script on entry point

--- 1-Bandit/hack_away.sh
+++ 1-Bandit/hack_away.sh
@@ -5,6 +5,6 @@ export REMOTE_HOST=bandit.labs.overthewire.org
 echo "Hacking level 0"
 export REMOTE_USER=bandit0
 export REMOTE_PASSWORD=bandit0
-export SCRIPT="level0/hack.sh"
+export SCRIPT="level0/steps.sh"
 export LEVEL_1_PASS=$(./connect.sh)
 echo "Password obtained for level 1: $LEVEL_1_PASS"
