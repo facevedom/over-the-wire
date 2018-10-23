7019c9860419a640918f25bc3815e2194f329b41
Felipe Acevedo <felipe.a.montoya@gmail.com>
2018-10-22 23:02:44 -0500

    changes entrypoint

---
+++ 1-Bandit/hack_away.sh
@@ -0,0 +1,9 @@
+export REMOTE_PORT=2220
+export REMOTE_HOST=bandit.labs.overthewire.org
+
+# level 0
+echo "Hacking level 0"
+export REMOTE_USER=bandit0
+export SCRIPT="level0/hack.sh"
+export LEVEL_1_PASS=$(./connect.sh)
+echo "Password obtained for level 1: $LEVEL_1_PASS"
--- 1-Bandit/level0/wrapper.sh
+++
@@ -1,5 +0,0 @@
-export REMOTE_PORT=2220
-export REMOTE_USER=bandit0
-export REMOTE_HOST=bandit.labs.overthewire.org
-export SCRIPT="$(pwd)/hack.sh"
-../connect.sh
