3e4fd77eb0343996103365fcb63a55f0d8184fc8
Felipe Acevedo <felipe.a.montoya@gmail.com>
2018-10-28 20:49:12 -0500

    adds level 6

---
+++ 1-Bandit/level6/steps.sh
@@ -0,0 +1,5 @@
+                                                            # owned by user bandit7 
+                                                            # and owned by group bandit6
+                                                            # and 33 bytes in size
+                                                            # and ignore errors
+cat $(find / 2>/dev/null -user bandit7 -group bandit6 -size 33c)    # find'em all
