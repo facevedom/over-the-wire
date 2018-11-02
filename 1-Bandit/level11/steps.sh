a2063d188571ca5173e7d9b8e7308a2531025bb4
Felipe Acevedo <felipe.a.montoya@gmail.com>
2018-11-01 22:21:19 -0500

    adds level 11

--- 1-Bandit/level11/steps.sh
+++ 1-Bandit/level11/steps.sh
@@ -1,1 +1,2 @@
-cat data.txt | tr 'A-Za-z' 'N-ZA-Mn-za-m'           # rotate alphabet ranges 13 positions
+                                                    # rotate alphabet ranges 13 positions
+cat data.txt | tr 'A-Za-z' 'N-ZA-Mn-za-m' | rev | cut -d ' ' -f 1
